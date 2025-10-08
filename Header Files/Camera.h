e4d7537482019accd1a58b056eab648195a00e2b
ParveenRawat <rawatparveen663@gmail.com>
2025-10-08 18:39:01 +0530

    Added : diffuse lighting

--- Header Files/Camera.h
+++ Header Files/Camera.h
@@ -13,6 +13,7 @@ public:
   glm::vec3 Position;
   glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
   glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
+  glm::mat4 cameraMatrix = glm::mat4(1.0f);
 
   int width;
   int height;
@@ -24,8 +25,10 @@ public:
 
   Camera(int width, int height, glm::vec3 postion);
 
-  void Matrix(float FOVdeg, float nearPlane, float farPlane, Shader &shader,
-              const char *uniform);
+  void updateMatrix(float FOVdeg, float nearPlane, float farPlane);
+
+  void Matrix(Shader &shader, const char *uniform);
+
   void Inputs(GLFWwindow *window);
 };
 
